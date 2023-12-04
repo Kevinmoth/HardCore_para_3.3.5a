bd8d913e32e7ece013ee96c62275d3af7841ce4a
Matecitos <31082992+Kevinmoth@users.noreply.github.com>
2023-12-04 14:06:25 -0300

    Update Seguimiento.lua

--- Seguimiento.lua
+++ Seguimiento.lua
@@ -2,12 +2,12 @@ local frame = CreateFrame("Frame")
 
 -- une al jugador a un canal global de seguimiento(puede usarse para agregar verificaciones)
 local function UnirseAlCanalDeSeguimiento()
-    JoinChannelByName("canaldeseguimiento", nil, ChatFrame1:GetID()) -- Cambia "canaldeseguimiento" al nombre que desees para tu canal.
+    JoinChannelByName("canaldeseguimiento", nil, ChatFrame1:GetID())
 end
 
--- Fse envia un mensaje al canal de siguimiento cuando el jugador inicie sesión.
+-- se envia un mensaje al canal de siguimiento cuando el jugador inicie sesión.
 local function EnviarMensajeDeInicioDeSesion()
-    SendChatMessage("¡Addon Hardcore activado!", "CHANNEL", nil, GetChannelName("canaldeseguimiento")) -- Cambia "canaldeseguimiento" al nombre de tu canal.
+    SendChatMessage("¡Addon Hardcore activado!", "CHANNEL", nil, GetChannelName("canaldeseguimiento")) 
 end
 
 frame:RegisterEvent("PLAYER_LOGIN")
